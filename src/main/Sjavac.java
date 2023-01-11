f3c6363e9bfb362d068f6dbd45f9fe8d89fc829b
ilays <ilay.soffer@mail.huji.ac.il>
2023-01-11 13:56:24 +0200

    1.1

---
+++ src/main/Sjavac.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.io.*;
+
+public class Sjavac {
+    public static void main(String[] args) {
+        try (FileReader fileReader = new FileReader(args[0]);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)){
+            while
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
