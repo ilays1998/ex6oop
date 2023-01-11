e3ae7179f418ddaaaa1ff80ef4827c02729387df
ilays <ilay.soffer@mail.huji.ac.il>
2023-01-11 14:16:41 +0200

    1.1

--- src/main/Sjavac.java
+++ src/main/Sjavac.java
@@ -3,10 +3,13 @@ package main;
 import java.io.*;
 
 public class Sjavac {
+    private static String line;
     public static void main(String[] args) {
         try (FileReader fileReader = new FileReader(args[0]);
              BufferedReader bufferedReader = new BufferedReader(fileReader)){
-            while
+            while((line = bufferedReader.readLine()) != null) {
+
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
